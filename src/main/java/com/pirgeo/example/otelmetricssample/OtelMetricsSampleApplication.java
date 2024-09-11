a1536bc39807be50f1cd1af55970772fd944e103
Georg P <georg.pirklbauer@dynatrace.com>
2024-09-11 14:45:48 +0200

    Spring Boot Starter (empty)

---
+++ src/main/java/com/pirgeo/example/otelmetricssample/OtelMetricsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.pirgeo.example.otelmetricssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelMetricsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtelMetricsSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pirgeo/example/otelmetricssample/OtelMetricsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pirgeo.example.otelmetricssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelMetricsSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
